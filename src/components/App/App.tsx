bcfe93b236329bea895f4e746f3377ec644226bc
Dima <96godin@gmail.com>
2025-10-05 16:56:02 +0300

    init

---
+++ src/components/App/App.tsx
@@ -0,0 +1,14 @@
+import MyName from "../MyName/MyName";
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <h1>Hi, you</h1>
+      <p>My first</p>
+      <MyName />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/MyName/MyName.tsx
@@ -0,0 +1,9 @@
+import "./MyName.css";
+
+export default function MyName() {
+  return (
+    <div>
+      <h1>Da</h1>
+    </div>
+  );
+}
